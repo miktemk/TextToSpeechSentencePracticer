b41ae9dad54cddcbf3d24cce223d3c5889538770
Mikhail Temkine <miktemk@gmail.com>
2017-09-01 00:21:00 -0400

    CommandLineArgs

---
+++ TextToSpeechSentencePracticer/Code/CommandLineArgs.cs
@@ -0,0 +1,52 @@
+﻿using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToSpeechSentencePracticer.Code
+{
+    public class CommandLineArgs
+    {
+        private readonly OptionSet options;
+
+        public CommandLineArgs(string[] args)
+        {
+            // these are the available options, not that they set the variables
+            options = new OptionSet {
+                { "l|lang=", $"TTS language. Default is {Lang}", x => Lang = x },
+                { "h|help", "show this message and exit", x => ShowHelp = x != null },
+                { "v", "increase debug message verbosity", x => IsVerbose = (x != null) },
+                //{ "f|file=", "Input file", x => Filename = x },
+            };
+
+            List<string> extra;
+            try
+            {
+                // parse the command line
+                extra = options.Parse(args);
+                Filename = extra.Skip(1).FirstOrDefault(); // NOTE: the 1st is the exe
+            }
+            catch (OptionException e)
+            {
+                // output some error message
+                Console.WriteLine("Error parsing command line arguments.");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `--help' for more information.");
+                ArgumentError = true;
+            }
+        }
+
+        public bool ArgumentError { get; private set; } = false;
+        public string Lang { get; private set; } = "en";
+        public bool ShowHelp { get; private set; } = false;
+        public bool IsVerbose { get; private set; } = false;
+        public string Filename { get; private set; }
+
+        public void PrintHelpStringToConsole()
+        {
+            options.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
--- TextToSpeechSentencePracticer/ViewModel/MainViewModel.cs
+++ TextToSpeechSentencePracticer/ViewModel/MainViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using GalaSoft.MvvmLight;
 using Miktemk.TextToSpeech.Core;
+using System;
+using System.IO;
+using TextToSpeechSentencePracticer.Code;
 
 namespace TextToSpeechSentencePracticer.ViewModel
 {
@@ -12,6 +15,30 @@ namespace TextToSpeechSentencePracticer.ViewModel
         {
             CurText = "xello mexicana YEHA its the eye!!!!!";
             CurHighlightRegion = new WordHighlight(6, 7);
+
+            var args = new CommandLineArgs(Environment.GetCommandLineArgs());
+            if (args.ArgumentError)
+            {
+                System.Windows.Application.Current.Shutdown();
+                return;
+            }
+            if (args.ShowHelp)
+            {
+                args.PrintHelpStringToConsole();
+                System.Windows.Application.Current.Shutdown();
+                return;
+            }
+            if (args.Filename == null)
+            {
+                Console.WriteLine("Please specify filename as command line argument");
+                System.Windows.Application.Current.Shutdown();
+                return;
+            }
+
+            // TODO: this shit
+            var allLines = File.ReadAllLines(args.Filename);
+
+            CurText = allLines[4];
         }
 
 
